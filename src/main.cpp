a5fa5a73edc4f917d3cdd439b4409b15a3ed81df
Gegel85 <Gegel85@laposte.net>
2019-10-26 11:08:12 +0200

    Fixed error box on Linux

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include "Game.hpp"
 #include "Logger.hpp"
 #include "Loader.hpp"
@@ -9,8 +8,8 @@
 #include <windows.h>
 #else
 #include <SFML/Graphics.hpp>
-#define ICON_ERROR_PATH "data/icon/error.png"
-#define MB_ICONERROR 0
+#define ICON_ERROR_PATH "assets/box/error.png"
+#define MB_ICONERROR 1
 #endif
 
 namespace TouhouFanGame
@@ -26,89 +25,70 @@ namespace TouhouFanGame
 #else
 	int	dispMsg(const std::string &title, const std::string &content, int variate)
 	{
-/*		char			**buttons = NULL;
+		std::vector<std::string>buttons;
 		int			clicked = -1;
-		sfRenderWindow		*win;
-		sfVideoMode		mode = {700, 220, 32};
-		sfEvent			event;
-		sfRectangleShape	*rect = sfRectangleShape_create();
-		sfFont			*font = sfFont_createFromFile("data/fonts/arial.ttf");
-		sfText			*text = sfText_create();
-		int			nbOfButtons = 0;
+		sf::RenderWindow	win{{700, 220}, title, sf::Style::Titlebar | sf::Style::Close};
+		sf::Event		event;
+		sf::RectangleShape	rect;
+		sf::Font		font;
+		sf::Text		text;
 		int			buttonClicked = 0;
-		sfTexture		*texture = NULL;
-		sfSprite		*sprite = sfSprite_create();
+		sf::Texture		texture;
+		sf::Sprite		sprite;
 		bool			hasIcon = false;
 
-		if (!font) {
-			printf("%s: Cannot load default font !\nDisplaying the message in the console:\n\n", ERROR_BEG);
-			printf("%s\n%s\n", title, content);
+		if (!font.loadFromFile("assets/arial.ttf")) {
+			logger.error("Cannot load default font !");
+			logger.error("Displaying the message in the console");
+			logger.error(title);
+			logger.error(content);
 		}
 		if (variate & MB_ICONERROR) {
 			hasIcon = true;
-			texture = sfTexture_createFromFile(ICON_ERROR_PATH, NULL);
-			sfSprite_setTexture(sprite, texture, true);
-			sfSprite_setPosition(sprite, (sfVector2f){20, 30});
+			texture.loadFromFile(ICON_ERROR_PATH);
+			sprite.setTexture(texture, true);
+			sprite.setPosition(20, 30);
 		}
 		if (variate & MB_YESNO) {
-			buttons = (char *[2]) {"Yes", "No"};
-			nbOfButtons = 2;
-		} else {
-			buttons = (char *[1]) {"OK"};
-			nbOfButtons = 1;
-		}
-		win = sfRenderWindow_create(mode, title ? title : "null", sfTitlebar | sfClose, NULL);
-		if (text && font)
-			sfText_setFont(text, font);
-		if (!win)
-			return (-1);
-		if (text)
-			sfText_setCharacterSize(text, 15);
-		while (sfRenderWindow_isOpen(win)) {
-			while (sfRenderWindow_pollEvent(win, &event)) {
-				if (event.type == sfEvtClosed)
-					sfRenderWindow_close(win);
-				if (event.type == sfEvtMouseButtonPressed) {
-					if (event.mouseButton.button == sfMouseLeft && event.mouseButton.y >= 150 && event.mouseButton.y <= 170) {
-						buttonClicked = (event.mouseButton.x - 400 + 70 * nbOfButtons) / 70;
-						if (buttonClicked >= 0 && buttonClicked < nbOfButtons) {
-							clicked = nbOfButtons == 2 ? (buttonClicked == 0 ? 6 : 5) : 0;
-							sfRenderWindow_close(win);
+			buttons.emplace_back("Yes");
+			buttons.emplace_back("No");
+		} else
+			buttons.emplace_back("OK");
+		text.setFont(font);
+		text.setCharacterSize(15);
+		while (win.isOpen()) {
+			while (win.pollEvent(event)) {
+				if (event.type == sf::Event::Closed)
+					win.close();
+				if (event.type == sf::Event::MouseButtonPressed) {
+					if (event.mouseButton.button == sf::Mouse::Left && event.mouseButton.y >= 150 && event.mouseButton.y <= 170) {
+						buttonClicked = (event.mouseButton.x - 400 + 70 * buttons.size()) / 70;
+						if (buttonClicked >= 0 && buttonClicked < buttons.size()) {
+							clicked = buttons.size() == 2 ? (buttonClicked == 0 ? 6 : 5) : 0;
+							win.close();
 						}
 					}
 				}
 			}
-			sfRenderWindow_clear(win, (sfColor){200, 200, 200, 255});
-			if (rect) {
-				sfRectangleShape_setFillColor(rect, (sfColor){255, 255, 255, 255});
-				sfRectangleShape_setSize(rect, (sfVector2f){60, 20});
-			}
-			if (text) {
-				sfText_setColor(text, (sfColor){0, 0, 0, 255});
-				sfText_setPosition(text, (sfVector2f){10 + 60 * hasIcon, 40});
-				sfText_setString(text, content ? content : "(null)");
-				sfRenderWindow_drawText(win, text, NULL);
-			}
+			win.clear({200, 200, 200, 255});
+			rect.setFillColor({255, 255, 255, 255});
+			rect.setSize({60, 20});
+			text.setFillColor({0, 0, 0, 255});
+			text.setPosition(10 + 60 * hasIcon, 40);
+			text.setString(content);
+			win.draw(text);
 			if (hasIcon)
-				sfRenderWindow_drawSprite(win, sprite, NULL);
-			for (int i = 0; i < nbOfButtons; i++) {
-				if (rect) {
-					sfRectangleShape_setPosition(rect, (sfVector2f){400 - 70 * (nbOfButtons - i), 150});
-					sfRenderWindow_drawRectangleShape(win, rect, NULL);
-				}
-				if (text) {
-					sfText_setString(text, buttons[i]);
-					sfText_setPosition(text, (sfVector2f){410 - 70 * (nbOfButtons - i), 150});
-					sfRenderWindow_drawText(win, text, NULL);
-				}
+				win.draw(sprite);
+			for (size_t i = 0; i < buttons.size(); i++) {
+				rect.setPosition(400 - 70 * (buttons.size() - i), 150);
+				win.draw(rect);
+				text.setString(buttons[i]);
+				text.setPosition(410 - 70 * (buttons.size() - i), 150);
+				win.draw(text);
 			}
-			sfRenderWindow_display(win);
+			win.display();
 		}
-		sfRenderWindow_destroy(win);
-		sfFont_destroy(font);
-		sfText_destroy(text);
-		sfRectangleShape_destroy(rect);
-		return (clicked);*/
+		return (clicked);
 	}
 #endif
 

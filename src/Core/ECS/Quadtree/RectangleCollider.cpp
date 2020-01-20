bcc026ad248ec3a786853a3187472ee43d1ed32e
AnonymusRaccoon <tr.tristan-roux@outlook.fr>
2020-01-20 13:49:47 +0100

    Oups

--- src/Core/ECS/Quadtree/RectangleCollider.cpp
+++ src/Core/ECS/Quadtree/RectangleCollider.cpp
@@ -69,6 +69,6 @@ namespace TouhouFanGame::ECS::Quadtree
 
 	void RectangleCollider::serialize(std::ostream &stream) const
 	{
-		stream << "1 " << this->rect << '\0'
+		stream << "1 " << this->rect << '\0';
 	}
 }

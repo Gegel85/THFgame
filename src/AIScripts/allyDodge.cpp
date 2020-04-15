f601e3f5c78f3b2af123a625cd0c7a16fb4c7321
Gegel85 <Gegel85@laposte.net>
2020-04-15 15:41:45 +0200

    Started ais base code

---
+++ src/AIScripts/allyDodge.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Gegel85 on 15/04/2020.
+//
+
+#include <vector>
+#include "../Core/ECS/Core.hpp"
+#include "../Core/Input/Input.hpp"
+#include "../Core/Resources/Map.hpp"
+
+extern "C"
+{
+	std::vector<TouhouFanGame::Input::Action> getAIActions(TouhouFanGame::ECS::Core &, TouhouFanGame::Map &) {
+		return {};
+	}
+}
